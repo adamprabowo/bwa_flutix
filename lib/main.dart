644f33b0bbe38158b388487a639ecbb482838650
adampra28 <adamprabowo00@gmail.com>
2020-08-06 13:46:47 +0700

    Menambahkan page bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:bwa_flutix/services/services.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 
 import 'ui/pages/pages.dart';
+import 'bloc/blocs.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,9 +15,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return StreamProvider.value(
       value: AuthServices.userStream,
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Wrapper(),
+      child: MultiBlocProvider(
+        providers: [BlocProvider(create: (_) => PageBloc())],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: Wrapper(),
+        ),
       ),
     );
   }
--- lib/shared/shared_value.dart
+++ lib/shared/shared_value.dart
@@ -2,3 +2,5 @@ part of 'shared.dart';
 
 String apiKey = "153de244dac5795eb0232512f015901d";
 String imageBaseURL = "https://image.tmdb.org/t/p/";
+
+PageEvent prevPageEvent;
